1413f602f29ad2f436ce206807035b0d25692284
Davi <davibnovaes@hotmail.com>
2022-02-08 23:31:26 -0300

    adjust code for deploy on heroku

--- src/database.js
+++ src/database.js
@@ -3,11 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-/* let db;
-mongoClient.connect(() => {
-    db = mongoClient.db("my-wallet");
-});
- */
 
 await mongoClient.connect()
 const db = mongoClient.db('my-wallet')
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-app.listen(5000, () => {
-  console.log("Server is litening on port http://localhost:5000")
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
 });
